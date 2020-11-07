fbc272c7528dcbcd4bcbdcab264be8279700f554
andrescuello7 <andrescuellotrabajo@gmail.com>
2020-11-06 23:57:22 -0300

    v-7

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const usersTable = document.getElementById('usersTable');
 const icon= document.getElementById('body');
 const icono= document.getElementById('icono');
 const crearElement= document.getElementById('crear');
+const everyTime= document.getElementById('todo');
+const mostrarUI= document.getElementById('mostrar');
 const users = [];
 
 
@@ -43,7 +45,8 @@ button.onclick = (e) => {
         Dos: Dos,
         division: division,
         id: generateId(),
-        createdAt: Date.now()
+        createdAt: Date.now(),
+        everyTime: everyTime
     })
     const usersJson = JSON.stringify(users);
     localStorage.setItem('users', usersJson);
@@ -52,11 +55,7 @@ button.onclick = (e) => {
     formUser.reset();
     displayUser();
 }
-function papi(){
-    // formUser.style.color= 'green';
-    // crearElement.style.background ='none';
-    // const osa=`<p>Hola como estas</p>`
-}
+
 function displayUser() {
     const users = JSON.parse(localStorage.getItem('users')) || [];
     const rows = [];
@@ -65,15 +64,21 @@ function displayUser() {
         const createdAt = new Date(user.createdAt)
         const tr = `
         <form >
-            <div class="formularioDeOrigen"> 
-            <p class="hl"><b>${user.division}</b></p>
-            <h5 class="tituloo">${user.Uno  || ''}</h5>
-            <br>
-            <div class="origen">
-              <button type="button" class="btn btn-info" data-toggle="modal" data-target="#modal${user.id}">Mostrar</button>
-              <button onclick="deleteUser('${user.id}')" class="btn btn-danger">Eliminar</button>
-            </div>
-            </div>
+                    <!-- Esto es otra cosa -->
+
+                <div class="formularioDeOrigen"> 
+                <p class="hl"><b>${user.division}</b></p>
+                <h5 class="tituloo">${user.Uno  || ''}</h5>
+                <br>
+                <div class="origen">
+                <button type="button" class="btn btn-info" data-toggle="modal" data-target="#modal${user.id}"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chat-right-text-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M16 2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h9.586a1 1 0 0 1 .707.293l2.853 2.853a.5.5 0 0 0 .854-.353V2zM3.5 3a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5z"/>
+                </svg></button>
+                <button onclick="deleteUser('${user.id}')" class="btn btn-danger"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
+                </svg></button>
+                </div>
+                </div>
 
             <!-- Modal -->
                 <div class="modal fade" id="modal${user.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -86,10 +91,10 @@ function displayUser() {
                                 </button>
                             </div>
                             <div class="modal-body">
-                                <p> ${user.Dos}</p>
+                                <textarea class="form-control" id="dos" rows="3" maxlength="500" placeholder="Ingrese Tarea"> ${user.Dos}</textarea>
                             </div>
                             <div id="datee" class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Guardar Cambios</button>
                                 <p class="data">Fecha: ${createdAt.toLocaleString()}</p>
                             </div>
                         </div>
@@ -109,4 +114,3 @@ function deleteUser(userId) {
     localStorage.setItem('users', usersJson);
     displayUser();
 }
-
